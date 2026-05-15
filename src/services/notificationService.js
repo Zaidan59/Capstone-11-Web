f2548fe7f8a70393127b75de3ec2301ac2fb51ae
Muhammad Zaidan <2410512043@mahasiswa.upnvj.ac.id>
2026-05-15 17:18:11 +0700

    Revert "Revert "Feat/frontend cleanup""

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -1,13 +1,7 @@
 import api from './api'
- 
-// Ambil semua notifikasi berdasarkan sekolahId yang sedang login
-export const getNotificationsBySekolahId = (sekolahId) =>
-  api.get(`/notifications/sekolah/${sekolahId}`)
- 
-// Tandai satu notifikasi sebagai sudah dibaca
-export const markAsRead = (notifId) =>
-  api.patch(`/notifications/${notifId}/read`)
- 
-// Tandai semua notifikasi milik sekolah sebagai sudah dibaca
-export const markAllAsRead = (sekolahId) =>
-  api.patch(`/notifications/sekolah/${sekolahId}/read-all`)
+
+export const getNotificationsBySppgId = (sppgId) =>
+  api.get('/notifikasi', { params: { sppgId } })
+
+export const getNotificationsBySchoolId = (schoolId) =>
+  api.get('/notifikasi', { params: { schoolId } })
